4d58f7f72c6b14e3fb31f15222bd49e0a4bda4a4
César Urbiña <cesarurb.dev@gmail.com>
2023-10-28 15:03:58 -0500

    Add project files.

---
+++ src/Tarker.Booking.Domain/Entities/Customer/CustomerEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace Tarker.Booking.Domain.Entities.Customer
+{
+    public class CustomerEntity
+    {
+        public int CustomerId { get; set; }
+        public string FullName { get; set; }
+        public string DocumentNumber { get; set; }
+    }
+}
